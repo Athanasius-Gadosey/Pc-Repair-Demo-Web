c112bc9ba139e91c2c84103a298f1730f7f268a3
Gadosey <44038699+Athanasius-Gadosey@users.noreply.github.com>
2022-07-09 03:16:01 +0000

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,39 @@ app.get('/about', function(req, res){
     res.render('about')
 });
 
+app.get('/contact', function(req, res){
+    res.render('contact');
+});
+
+app.post('/', function(req, res) {
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'athanasuisgadosey@gmail.com',
+            pass: 'liveping@123'
+        }
+    });
+
+    let mailOptions = {
+        from: 'Athanasuis Gadosey <athanasuisgadosey@gmail.com>',
+        to: 'tecsol@yahoo.com',
+        subject: 'Technical Solution',
+        text: 'The submission as the following issues... name: ' + req.body.name + 'email: '+ req.body.email + 'message: '+ req.body.message,
+        html: '<p>The submission as the following issues...</p><ul><li>name: '+ req.body.name+'</li><li>email: ' + req.body.email +'</li><li>message: ' + req.body.message + '</li></ul>'
+    };
+
+    transporter.sendMail(mailOptions, function(err, info) {
+        if(err) {
+            console.log(err);
+            res.redirect('/');
+        }
+        else{
+            console.log('Message sent:' + info.response);
+            res.redirect('/');
+        }
+    });
+
+});
 
 // Ports listen
 app.listen(2000, function(req, res){
